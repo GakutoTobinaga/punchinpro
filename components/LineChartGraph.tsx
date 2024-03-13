1b02fd7f6219a6be2b94810960a7ac4ecac37860
gtobinaga-sparkleai <gtobinaga@sparkleai.co.jp>
2024-03-14 00:03:57 +0900

    exc prittier

--- components/LineChartGraph.tsx
+++ components/LineChartGraph.tsx
@@ -1,25 +1,23 @@
 import { LineChart } from '@tremor/react';
 
 type EnergyLevelData = {
-    date: string;
-    元気レベル: number | null;
-  };
-  
-  // コンポーネントのプロップスの型定義
+  date: string;
+  元気レベル: number | null;
+};
+
+// コンポーネントのプロップスの型定義
 type EnergyProps = {
   energyLevels: EnergyLevelData[];
 };
 
-export function LineChartGraph( {energyLevels} :  EnergyProps ) {
+export function LineChartGraph({ energyLevels }: EnergyProps) {
   return (
     <LineChart
-    categories={[
-        '元気レベル'
-    ]}
-    className="h-72"
-    data={energyLevels}
-    index="date"
-    onValueChange={function noRefCheck(){}}
+      categories={['元気レベル']}
+      className="h-72"
+      data={energyLevels}
+      index="date"
+      onValueChange={function noRefCheck() {}}
     />
   );
 }
--- components/UserAllAttendance.tsx
+++ components/UserAllAttendance.tsx
@@ -45,11 +45,11 @@ const AttendanceTable = ({
   adminSessionToken,
   fullname,
 }: AttendanceTableProps) => {
-  const energyLevels = data.map(item => ({
-    "元気レベル": item.energyLevel !== null ? item.energyLevel : 0,
-    date: formatDateString(item.date)
+  const energyLevels = data.map((item) => ({
+    元気レベル: item.energyLevel !== null ? item.energyLevel : 0,
+    date: formatDateString(item.date),
   }));
-  
+
   const handleDeleteAttendance = async (
     attendanceId: number,
     userId: number
@@ -69,27 +69,32 @@ const AttendanceTable = ({
       case 1:
         return {
           label: '絶不調',
-          color: 'bg-red-500 border-red-500 hover:bg-red-600 border-red-600 cursor-not-allowed',
+          color:
+            'bg-red-500 border-red-500 hover:bg-red-600 border-red-600 cursor-not-allowed',
         };
       case 2:
         return {
           label: '不調',
-          color: 'bg-orange-500 border-orange-500 hover:bg-orange-600 border-orange-600 cursor-not-allowed',
+          color:
+            'bg-orange-500 border-orange-500 hover:bg-orange-600 border-orange-600 cursor-not-allowed',
         };
       case 3:
         return {
           label: '普通',
-          color: 'bg-yellow-500 border-yellow-500 hover:bg-yellow-600 border-yellow-600 cursor-not-allowed',
+          color:
+            'bg-yellow-500 border-yellow-500 hover:bg-yellow-600 border-yellow-600 cursor-not-allowed',
         };
       case 4:
         return {
           label: '良好',
-          color: 'bg-blue-500 border-blue-500 hover:bg-blue-600 border-blue-600 cursor-not-allowed',
+          color:
+            'bg-blue-500 border-blue-500 hover:bg-blue-600 border-blue-600 cursor-not-allowed',
         };
       case 5:
         return {
           label: '絶好調',
-          color: 'bg-green-500 border-green-500 hover:bg-green-600 border-green-600 cursor-not-allowed',
+          color:
+            'bg-green-500 border-green-500 hover:bg-green-600 border-green-600 cursor-not-allowed',
         };
       default:
         return {
@@ -101,73 +106,75 @@ const AttendanceTable = ({
 
   return (
     <>
-    <LineChartGraph energyLevels={energyLevels}/>
-    <Card>
-      <Title className="text-xl">
-        {adminSessionToken ? `${fullname}` : 'Your Attendance Data'}
-      </Title>
-      <Table className="mt-10">
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell>日付</TableHeaderCell>
-            <TableHeaderCell>出勤時間</TableHeaderCell>
-            <TableHeaderCell>退勤時間</TableHeaderCell>
-            {adminSessionToken && <TableHeaderCell></TableHeaderCell>}
-            <TableHeaderCell></TableHeaderCell>
-            {adminSessionToken && <TableHeaderCell></TableHeaderCell>}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((item, index) => (
-            <TableRow key={index}>
-              <TableCell>{formatDateString(item.date)}</TableCell>
-              <TableCell>
-                {new Date(item.startTime).toLocaleTimeString('ja-JP')}
-              </TableCell>
-              <TableCell>
-                {item.endTime
-                  ? new Date(item.endTime).toLocaleTimeString('ja-JP')
-                  : '未定'}
-              </TableCell>
-              {adminSessionToken && (
-                <TableCell>
-                  <Link
-                    href={`/mypage/${item.userId}/description/${item.id}`}
-                    passHref
-                  >
-                    <Button size="xs">詳細</Button>
-                  </Link>
-                </TableCell>
-              )}
-              <TableCell>
-                {/* getEnergyLevelDetails関数からラベルと色を取得 */}
-                {(() => {
-                  const { label, color } = getEnergyLevelDetails(
-                    item.energyLevel
-                  );
-                  return (
-                    <Button className={`text-white ${color}`} size="xs">
-                      {label}
-                    </Button>
-                  );
-                })()}
-              </TableCell>
-              {adminSessionToken && (
-                <TableCell>
-                  <Button
-                    size="xs"
-                    className="bg-red-500 hover:bg-red-500 border-red-500 cursor-pointer" // ホバー時の色変更を無効化
-                    onClick={() => handleDeleteAttendance(item.id, item.userId)}
-                  >
-                    削除
-                  </Button>
-                </TableCell>
-              )}
+      <LineChartGraph energyLevels={energyLevels} />
+      <Card>
+        <Title className="text-xl">
+          {adminSessionToken ? `${fullname}` : 'Your Attendance Data'}
+        </Title>
+        <Table className="mt-10">
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>日付</TableHeaderCell>
+              <TableHeaderCell>出勤時間</TableHeaderCell>
+              <TableHeaderCell>退勤時間</TableHeaderCell>
+              {adminSessionToken && <TableHeaderCell></TableHeaderCell>}
+              <TableHeaderCell></TableHeaderCell>
+              {adminSessionToken && <TableHeaderCell></TableHeaderCell>}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Card>
+          </TableHead>
+          <TableBody>
+            {data.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>{formatDateString(item.date)}</TableCell>
+                <TableCell>
+                  {new Date(item.startTime).toLocaleTimeString('ja-JP')}
+                </TableCell>
+                <TableCell>
+                  {item.endTime
+                    ? new Date(item.endTime).toLocaleTimeString('ja-JP')
+                    : '未定'}
+                </TableCell>
+                {adminSessionToken && (
+                  <TableCell>
+                    <Link
+                      href={`/mypage/${item.userId}/description/${item.id}`}
+                      passHref
+                    >
+                      <Button size="xs">詳細</Button>
+                    </Link>
+                  </TableCell>
+                )}
+                <TableCell>
+                  {/* getEnergyLevelDetails関数からラベルと色を取得 */}
+                  {(() => {
+                    const { label, color } = getEnergyLevelDetails(
+                      item.energyLevel
+                    );
+                    return (
+                      <Button className={`text-white ${color}`} size="xs">
+                        {label}
+                      </Button>
+                    );
+                  })()}
+                </TableCell>
+                {adminSessionToken && (
+                  <TableCell>
+                    <Button
+                      size="xs"
+                      className="bg-red-500 hover:bg-red-500 border-red-500 cursor-pointer" // ホバー時の色変更を無効化
+                      onClick={() =>
+                        handleDeleteAttendance(item.id, item.userId)
+                      }
+                    >
+                      削除
+                    </Button>
+                  </TableCell>
+                )}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
     </>
   );
 };
